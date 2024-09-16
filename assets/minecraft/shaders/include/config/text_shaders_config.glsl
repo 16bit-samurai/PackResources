e0c0da3ebe00e43de02054b7f908b67a4b2da640
Acappellia <lishangyu1105@gmail.com>
2024-09-16 16:57:05 +0800

    add new furniture

---
+++ assets/minecraft/shaders/include/config/text_shaders_config.glsl
@@ -0,0 +1,84 @@
+TEXT_EFFECT(255, 250, 0) {
+	applyRainbow(-3.0);
+}
+
+TEXT_EFFECT(255, 250, 4) {
+	applyRainbow2(-10.0, 5);
+}
+
+TEXT_EFFECT(255, 250, 8) {
+	applyRainbow2(-10.0, 50);
+}
+
+TEXT_EFFECT(255, 250, 12) {
+	applyRainbow2(-10.0, 500);
+}
+
+TEXT_EFFECT(255, 254, 0) {
+	applyGradient(rgb(201, 255, 191), rgb(255, 175, 189), -1.0);
+	//pink_green
+}
+
+TEXT_EFFECT(255, 254, 4) {
+	applyGradient(rgb(255, 252, 0), rgb(255, 255, 255), -1.0);
+	//yellow_white
+}
+
+TEXT_EFFECT(255, 254, 8) {
+	applyGradient(rgb(189, 195, 199), rgb(44, 62, 80), -1.0);
+	//gray
+}
+
+TEXT_EFFECT(255, 254, 12) {
+	applyGradient(rgb(101, 78, 163), rgb(234, 175, 200), -1.0);
+	//violet
+}
+
+TEXT_EFFECT(255, 254, 16) {
+	applyGradient(rgb(241, 39, 17), rgb(245, 175, 25), -1.0);
+	//lava
+}
+
+TEXT_EFFECT(255, 254, 20) {
+	applyGradient(rgb(252, 92, 125), rgb(106, 130, 251), -1.0);
+	//blue_pink
+}
+
+TEXT_EFFECT(255, 254, 24) {
+	applyGradient(rgb(0, 176, 155), rgb(150, 201, 61), -1.0);
+	//green
+}
+
+TEXT_EFFECT(255, 254, 28) {
+	applyGradient(rgb(116, 235, 213), rgb(172, 182, 229), -1.0);
+	//light_blue_pink
+}
+
+TEXT_EFFECT(255, 254, 32) {
+	applyGradient(rgb(225, 238, 195), rgb(240, 80, 83), -1.0);
+	//pink
+}
+
+TEXT_EFFECT(255, 254, 36) {
+	applyGradient(rgb(67, 198, 172), rgb(248, 255, 174), -1.0);
+	//light_green
+}
+
+TEXT_EFFECT(255, 254, 40) {
+	applyGradient(rgb(252, 234, 187), rgb(248, 181, 0), -1.0);
+	//orange
+}
+
+TEXT_EFFECT(255, 254, 44) {
+	applyGradient(rgb(28, 146, 210), rgb(242, 252, 254), -1.0);
+	//pure_light_blue
+}
+
+TEXT_EFFECT(255, 254, 48) {
+	applyGradient(rgb(142, 14, 0), rgb(31, 28, 24), -1.0);
+	//dark_red
+}
+
+TEXT_EFFECT(255, 254, 254) {
+	applyRemoveShadow();
+}
